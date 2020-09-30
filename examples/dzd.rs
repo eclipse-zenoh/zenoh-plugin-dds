5faa35f36a6165445f77932bff2d546d44b18650
kydos <angelo@icorsaro.net>
2020-09-30 11:30:54 +0200

    ensure format compliant with rust-fmt

--- examples/dzd.rs
+++ examples/dzd.rs
@@ -1,16 +1,16 @@
 #![feature(vec_into_raw_parts)]
 
-use zplugin_dds::*;
-use clap::{App, Arg};
-use futures::prelude::*;
-use zenoh::net::*;
-use cyclors::*;
-use std::collections::HashMap;
-use log::{debug};
-use std::sync::Arc;
 use async_std::task;
-use std::sync::mpsc::{channel, Receiver, Sender};
+use clap::{App, Arg};
+use cyclors::*;
+use futures::prelude::*;
+use log::debug;
+use std::collections::HashMap;
 use std::ffi::CString;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::Arc;
+use zenoh::net::*;
+use zplugin_dds::*;
 
 fn parse_args() -> (Config, String) {
     let args = App::new("dzd zenoh router for DDS")
@@ -30,7 +30,8 @@ fn parse_args() -> (Config, String) {
         )
         .get_matches();
 
-    let scope: String = args.value_of("scope")
+    let scope: String = args
+        .value_of("scope")
         .map(|s| String::from(s))
         .or_else(|| Some(String::from("")))
         .unwrap();
@@ -40,31 +41,32 @@ fn parse_args() -> (Config, String) {
             args.values_of("peer")
                 .map(|p| p.collect())
                 .or_else(|| Some(vec![]))
-                .unwrap())
+                .unwrap(),
+        )
         .add_listeners(
             args.values_of("listener")
                 .map(|p| p.collect())
                 .or_else(|| Some(vec![]))
-                .unwrap())
+                .unwrap(),
+        )
         .mode(
             args.value_of("mode")
                 .map(|m| Config::parse_mode(m))
                 .unwrap()
-                .unwrap())
+                .unwrap(),
+        )
         .local_routing(false)
         .mode(whatami::CLIENT);
 
     (config, scope)
 }
 
-
 #[async_std::main]
 async fn main() {
     env_logger::init();
     let (config, scope) = parse_args();
-    let dp = unsafe {
-        dds_create_participant(DDS_DOMAIN_DEFAULT, std::ptr::null(), std::ptr::null())
-    };
+    let dp =
+        unsafe { dds_create_participant(DDS_DOMAIN_DEFAULT, std::ptr::null(), std::ptr::null()) };
     let z = Arc::new(open(config, None).await.unwrap());
     let (tx, rx): (Sender<MatchedEntity>, Receiver<MatchedEntity>) = channel();
     run_discovery(dp, tx);
@@ -73,12 +75,21 @@ async fn main() {
     let mut wr_map = HashMap::<String, dds_entity_t>::new();
     let _zsub_map = HashMap::<String, CallbackSubscriber>::new();
     while let Ok(me) = rx.recv() {
-        match me  {
-            MatchedEntity::DiscoveredPublication {topic_name, type_name, keyless, partition, qos} => {
-                debug!("DiscoveredPublication({}, {}, {:?}", topic_name, type_name, partition);
+        match me {
+            MatchedEntity::DiscoveredPublication {
+                topic_name,
+                type_name,
+                keyless,
+                partition,
+                qos,
+            } => {
+                debug!(
+                    "DiscoveredPublication({}, {}, {:?}",
+                    topic_name, type_name, partition
+                );
                 let key = match partition {
                     Some(p) => format!("{}/{}/{}", scope, p, topic_name),
-                    None => format!("{}/{}", scope, topic_name)
+                    None => format!("{}/{}", scope, topic_name),
                 };
                 debug!("Declaring resource {}", key);
                 match rd_map.get(&key) {
@@ -89,38 +100,75 @@ async fn main() {
                         let _ = z.declare_publisher(&rid).await;
                         rid_map.insert(key.clone(), nrid);
                         debug!("Creating Forwarding Reader for: {}", key);
-                        let dr: dds_entity_t =
-                            create_forwarding_dds_reader(dp, topic_name, type_name, keyless, qos, rid, z.clone());
+                        let dr: dds_entity_t = create_forwarding_dds_reader(
+                            dp,
+                            topic_name,
+                            type_name,
+                            keyless,
+                            qos,
+                            rid,
+                            z.clone(),
+                        );
                         rd_map.insert(key, dr);
-                    },
-                    _ => {
-                        debug!("Already forwarding matching subscription {} -- ignoring", topic_name);
                     }
-
+                    _ => {
+                        debug!(
+                            "Already forwarding matching subscription {} -- ignoring",
+                            topic_name
+                        );
+                    }
                 }
-            },
-            MatchedEntity::UndiscoveredPublication {topic_name, type_name, partition} => {
-                debug!("UndiscoveredPublication({}, {}, {:?}", topic_name, type_name, partition);
-            },
-            MatchedEntity::DiscoveredSubscription {topic_name, type_name, keyless, partition, qos} => {
-                debug!("DiscoveredSubscription({}, {}, {:?}", topic_name, type_name, partition);
+            }
+            MatchedEntity::UndiscoveredPublication {
+                topic_name,
+                type_name,
+                partition,
+            } => {
+                debug!(
+                    "UndiscoveredPublication({}, {}, {:?}",
+                    topic_name, type_name, partition
+                );
+            }
+            MatchedEntity::DiscoveredSubscription {
+                topic_name,
+                type_name,
+                keyless,
+                partition,
+                qos,
+            } => {
+                debug!(
+                    "DiscoveredSubscription({}, {}, {:?}",
+                    topic_name, type_name, partition
+                );
                 let key = match &partition {
                     Some(p) => format!("{}/{}/{}", scope, p, topic_name),
-                    None => format!("{}/{}", scope, topic_name)
+                    None => format!("{}/{}", scope, topic_name),
                 };
 
                 if let Some(wr) = match wr_map.get(&key) {
                     Some(_) => {
-                        debug!("The Subscription({}, {}, {:?} is aready handled, IGNORING", topic_name, type_name, partition);
+                        debug!(
+                            "The Subscription({}, {}, {:?} is aready handled, IGNORING",
+                            topic_name, type_name, partition
+                        );
                         None
-                    },
+                    }
                     None => {
-                        let wr = create_forwarding_dds_writer(dp, topic_name.clone(), type_name.clone(), keyless, qos);
+                        let wr = create_forwarding_dds_writer(
+                            dp,
+                            topic_name.clone(),
+                            type_name.clone(),
+                            keyless,
+                            qos,
+                        );
                         wr_map.insert(key.clone(), wr);
                         Some(wr)
                     }
                 } {
-                    debug!("The Subscription({}, {}, {:?} is new setting up zenoh and DDS endpoings", topic_name, type_name, partition);
+                    debug!(
+                        "The Subscription({}, {}, {:?} is new setting up zenoh and DDS endpoings",
+                        topic_name, type_name, partition
+                    );
                     let nrid = match rid_map.get(&key) {
                         Some(nrid) => *nrid,
                         None => {
@@ -135,7 +183,7 @@ async fn main() {
                         mode: SubMode::Push,
                         period: None,
                     };
-                    let  rsel = rkey.into();
+                    let rsel = rkey.into();
 
                     let zn = z.clone();
                     task::spawn(async move {
@@ -145,32 +193,48 @@ async fn main() {
                             let ton = topic_name.clone();
                             let tyn = type_name.clone();
                             task::spawn_blocking(move || {
-                            unsafe {
-                                let bs = d.payload.to_vec();
-                                // As per the Vec documentation (see https://doc.rust-lang.org/std/vec/struct.Vec.html#method.into_raw_parts)
-                                // the only way to correctly releasing it is to create a vec using from_raw_parts
-                                // and then have its destructor do the cleanup.
-                                // Thus, while tempting to just pass the raw pointer to cyclone and then free it from C,
-                                // that is not necessarily safe or guaranteed to be leak free.
-                                let (ptr, len, capacity) = bs.into_raw_parts();
-                                let cton = CString::new(ton).unwrap().as_ptr();
-                                let ctyn = CString::new(tyn).unwrap().as_ptr();
-                                let st = cdds_create_blob_sertopic(dp, cton as *mut std::os::raw::c_char, ctyn as *mut std::os::raw::c_char, keyless);
-                                let fwdp = cdds_ddsi_payload_create(st, ddsi_serdata_kind_SDK_DATA, ptr, len as u64);
-                                dds_writecdr(wr, fwdp as *mut ddsi_serdata);
-                                drop(Vec::from_raw_parts(ptr, len, capacity));
-                                cdds_sertopic_unref(st);
-                            };
-                        }).await;
+                                unsafe {
+                                    let bs = d.payload.to_vec();
+                                    // As per the Vec documentation (see https://doc.rust-lang.org/std/vec/struct.Vec.html#method.into_raw_parts)
+                                    // the only way to correctly releasing it is to create a vec using from_raw_parts
+                                    // and then have its destructor do the cleanup.
+                                    // Thus, while tempting to just pass the raw pointer to cyclone and then free it from C,
+                                    // that is not necessarily safe or guaranteed to be leak free.
+                                    let (ptr, len, capacity) = bs.into_raw_parts();
+                                    let cton = CString::new(ton).unwrap().as_ptr();
+                                    let ctyn = CString::new(tyn).unwrap().as_ptr();
+                                    let st = cdds_create_blob_sertopic(
+                                        dp,
+                                        cton as *mut std::os::raw::c_char,
+                                        ctyn as *mut std::os::raw::c_char,
+                                        keyless,
+                                    );
+                                    let fwdp = cdds_ddsi_payload_create(
+                                        st,
+                                        ddsi_serdata_kind_SDK_DATA,
+                                        ptr,
+                                        len as u64,
+                                    );
+                                    dds_writecdr(wr, fwdp as *mut ddsi_serdata);
+                                    drop(Vec::from_raw_parts(ptr, len, capacity));
+                                    cdds_sertopic_unref(st);
+                                };
+                            })
+                            .await;
                         }
                     });
                 }
-
-            },
-            MatchedEntity::UndiscoveredSubscription {topic_name, type_name, partition} => {
-                debug!("UndiscoveredSubscription({}, {}, {:?}", topic_name, type_name, partition);
-            },
-
+            }
+            MatchedEntity::UndiscoveredSubscription {
+                topic_name,
+                type_name,
+                partition,
+            } => {
+                debug!(
+                    "UndiscoveredSubscription({}, {}, {:?}",
+                    topic_name, type_name, partition
+                );
+            }
         }
     }
 }
