97ce11eefd2b5bccdb1f34ea0ad1da7137f1b847
kydos <angelo@icorsaro.net>
2021-02-18 19:36:16 +0100

    fix rust-fmt compains)

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let link_search_path: String = 
-        if let Ok(path) = std::env::var("CYCLONE_LIB") { format!("cargo:rustc-link-search={}", path) }
-        else { "cargo:rustc-link-search=/usr/local/lib".into() };
+    let link_search_path: String = if let Ok(path) = std::env::var("CYCLONE_LIB") {
+        format!("cargo:rustc-link-search={}", path)
+    } else {
+        "cargo:rustc-link-search=/usr/local/lib".into()
+    };
     println!("{}", link_search_path);
     println!("cargo:rustc-link-lib=cdds-util");
 }
