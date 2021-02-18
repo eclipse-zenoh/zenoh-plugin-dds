e21190243c5cd2c6b7d2a701a6c91d4c9f6776f9
kydos <angelo@icorsaro.net>
2021-02-18 19:23:46 +0100

    make sure to use the right link-path when CYCLONE_LIB is set (#23)

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-search=/usr/local/lib");
+    let link_search_path: String = 
+        if let Ok(path) = std::env::var("CYCLONE_LIB") { format!("cargo:rustc-link-search={}", path) }
+        else { "cargo:rustc-link-search=/usr/local/lib".into() };
+    println!("{}", link_search_path);
     println!("cargo:rustc-link-lib=cdds-util");
 }
