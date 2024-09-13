8cc190e0a6c6f2dd4e2065e85d7a5430d70c82db
Gabriele Baldoni <gabrik@users.noreply.github.com>
2024-09-13 14:56:12 +0200

    deps: align zenoh #1415 (#333)
    * deps: align zenoh #1415
    
    * deps: updated zenoh
    
    Signed-off-by: Gabriele Baldoni <gabriele.baldoni@gmail.com>
    
    ---------
    
    Signed-off-by: Gabriele Baldoni <gabriele.baldoni@gmail.com>

--- zenoh-bridge-dds/src/main.rs
+++ zenoh-bridge-dds/src/main.rs
@@ -257,13 +257,11 @@ async fn main() {
 
     // declare REST plugin if specified in conf
     if config.plugin("rest").is_some() {
-        plugins_mgr =
-            plugins_mgr.declare_static_plugin::<zenoh_plugin_rest::RestPlugin, &str>("rest", true);
+        plugins_mgr.declare_static_plugin::<zenoh_plugin_rest::RestPlugin, &str>("rest", true);
     }
 
     // declare DDS plugin
-    plugins_mgr =
-        plugins_mgr.declare_static_plugin::<zenoh_plugin_dds::DDSPlugin, &str>("dds", true);
+    plugins_mgr.declare_static_plugin::<zenoh_plugin_dds::DDSPlugin, &str>("dds", true);
 
     // create a zenoh Runtime.
     let mut runtime = match RuntimeBuilder::new(config)
