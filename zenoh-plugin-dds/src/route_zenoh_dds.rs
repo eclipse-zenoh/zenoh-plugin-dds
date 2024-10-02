7220e9d746ddcbbf81f1ef825a820cb8c2bc31b8
Geoff Martin <geoff.martin@zettascale.tech>
2024-10-02 15:16:05 +0100

    Updated Zenoh subscriber declaration to undeclare subscribers when references are dropped.

--- zenoh-plugin-dds/src/route_zenoh_dds.rs
+++ zenoh-plugin-dds/src/route_zenoh_dds.rs
@@ -186,6 +186,7 @@ impl RouteZenohDDS<'_> {
                 .zsession
                 .declare_subscriber(ke.clone())
                 .callback(subscriber_callback)
+                .undeclare_on_drop(true)
                 .allowed_origin(Locality::Remote) // Allow only remote publications to avoid loops
                 .querying()
                 .query_timeout(plugin.config.queries_timeout)
@@ -203,6 +204,7 @@ impl RouteZenohDDS<'_> {
                 .zsession
                 .declare_subscriber(ke.clone())
                 .callback(subscriber_callback)
+                .undeclare_on_drop(true)
                 .allowed_origin(Locality::Remote) // Allow only remote publications to avoid loops
                 .await
                 .map_err(|e| {
